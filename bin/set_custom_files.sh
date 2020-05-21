9db03ad349e03119b58d7011d62e5cb30f021c0a
Iva E. Popova <progressbg.python.course@gmail.com>
2020-05-21 15:28:06 +0300

    improve bin

--- bin/set_custom_files.sh
+++ bin/set_custom_files.sh
@@ -6,6 +6,9 @@ CUSTOM_CSS_FILE="$PWD/bin/custom.css"
 
 JUPY_CSS_FILE="$PWD/.venv/lib/python3.7/site-packages/notebook/static/custom/custom.css"
 if [ -f $JUPY_CSS_FILE ]; then
+  # backup
+  mv $JUPY_CSS_FILE "$JUPY_CSS_FILE.ori"
+  # replace
   cp $CUSTOM_CSS_FILE $JUPY_CSS_FILE
 else
    echo "File $JUPY_CSS_FILE does not exist."
@@ -17,6 +20,9 @@ CUSTOM_JS_FILE="$PWD/bin/custom.js"
 
 JUPY_JS_FILE="$PWD/.venv/lib/python3.7/site-packages/notebook/static/custom/custom.js"
 if [ -f $JUPY_JS_FILE ]; then
+  # backup
+  mv $JUPY_JS_FILE "$JUPY_JS_FILE.ori"
+  # replace
   cp $CUSTOM_JS_FILE $JUPY_JS_FILE
 else
    echo "File $JUPY_JS_FILE does not exist."
